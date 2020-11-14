efbe56fff1917831e5fabcbea69db35a10c7ee98
kym1924 <kym1924@naver.com>
2020-11-14 17:07:38 +0900

    Feat : setting BottomNavigationView

--- 29cm_Android/app/src/main/java/com/example/org29cm_android/ui/main/MainActivity.kt
+++ 29cm_Android/app/src/main/java/com/example/org29cm_android/ui/main/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.org29cm_android.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.org29cm_android.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navi.itemIconTintList=null
     }
 }
