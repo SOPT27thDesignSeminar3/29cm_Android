908ace02d698555121e2680157aad7ff269365a7
SSong-develop <thdgnsrl123@naver.com>
2020-11-30 19:28:23 +0900

    initial commit

--- 29cm_Android/app/src/main/java/com/example/org29cm_android/ui/home/HomeFragment.kt
+++ 29cm_Android/app/src/main/java/com/example/org29cm_android/ui/home/HomeFragment.kt
@@ -1,17 +1,18 @@
 package com.example.org29cm_android.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.org29cm_android.R
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        return inflater.inflate(R.layout.fragment_home,container,false)
     }
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 29cm_Android
+
+## Commit Convention
+
+### Branch : feature/이름철자
+
+#### ex) 훈기 : feature/hk
+
